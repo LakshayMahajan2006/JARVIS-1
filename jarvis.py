0c3af490e1ef059e082bdb6d4de48d67048c3eae
Zailta <mannanbhat60@gmail.com>
2020-10-04 12:15:24 +0530

    setting voice ids and checking functionality

--- jarvis.py
+++ jarvis.py
@@ -1,8 +1,15 @@
 import pyttsx3
-
+#sapi5 is a microsoft api for voice assistant
 engine = pyttsx3.init('sapi5')
+
 voices =  engine.getProperty('voices')
 print(voices)
+#setting id of voices, microsoft by default has 2 voices we can download more
+engine.setProperty('voice', voices[1].id)
+print(voices[0].id)
 
 def speak(audio):
-    pass
+    engine.say(audio)
+    engine.runAndWait()
+if __name__ == "__main__":
+    speak("hello Mannan how are you")
